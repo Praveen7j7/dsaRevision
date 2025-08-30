4b8f8a50019ba0751c8c74c1743e683c67ea3026
Praveen Jha <praveenworkforum@gmail.com>
2025-08-30 15:58:28 +0530

    Dp_update

---
+++ Dp/noAdjacentHighestSubSetSum.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long  long int ll;
+int main (){
+    ll n;
+    cin>>n;
+    ll a[n+1];
+    ll i=1;
+    while(i<=n)
+    {
+        cin>>a[i];
+        i++;
+    }
+    ll dp[n+1]={0};
+    ll pvd=0;
+    dp[1]=max(a[1],pvd);
+    dp[2]=max(a[2],max(a[1],pvd));
+
+    i=3;
+    while(i<=n){
+        dp[i]=max(dp[i-1],a[i]+dp[i-2]);
+        i++;
+    }
+    cout<<dp[n];
+
+
+    
+}
